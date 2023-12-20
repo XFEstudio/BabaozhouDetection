39707a692582d5c3640dbde50c1ddbba969d18e6
XFEstudio <mail@xfegzs.com>
2023-12-21 02:00:05 +0800

    添加项目文件。

---
+++ 八宝粥识别/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace 八宝粥识别
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cameraPictureBox = new PictureBox();
+            detectedPictureBox = new PictureBox();
+            detectActionButton = new Button();
+            selectPictureButton = new Button();
+            openImageFileDialog = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)cameraPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)detectedPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // cameraPictureBox
+            // 
+            cameraPictureBox.Location = new Point(29, 37);
+            cameraPictureBox.Name = "cameraPictureBox";
+            cameraPictureBox.Size = new Size(628, 551);
+            cameraPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            cameraPictureBox.TabIndex = 0;
+            cameraPictureBox.TabStop = false;
+            // 
+            // detectedPictureBox
+            // 
+            detectedPictureBox.Location = new Point(826, 37);
+            detectedPictureBox.Name = "detectedPictureBox";
+            detectedPictureBox.Size = new Size(628, 551);
+            detectedPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            detectedPictureBox.TabIndex = 1;
+            detectedPictureBox.TabStop = false;
+            // 
+            // detectActionButton
+            // 
+            detectActionButton.Location = new Point(1001, 622);
+            detectActionButton.Name = "detectActionButton";
+            detectActionButton.Size = new Size(267, 69);
+            detectActionButton.TabIndex = 2;
+            detectActionButton.Text = "识别";
+            detectActionButton.UseVisualStyleBackColor = true;
+            detectActionButton.Click += DetectActionButton_Click;
+            // 
+            // selectPictureButton
+            // 
+            selectPictureButton.Location = new Point(304, 819);
+            selectPictureButton.Name = "selectPictureButton";
+            selectPictureButton.Size = new Size(904, 51);
+            selectPictureButton.TabIndex = 3;
+            selectPictureButton.Text = "选择图片";
+            selectPictureButton.UseVisualStyleBackColor = true;
+            selectPictureButton.Click += SelectPictureButton_Click;
+            // 
+            // openImageFileDialog
+            // 
+            openImageFileDialog.Filter = "图片文件|*.*";
+            openImageFileDialog.Title = "选择要检测八宝粥的图片";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1484, 882);
+            Controls.Add(selectPictureButton);
+            Controls.Add(detectActionButton);
+            Controls.Add(detectedPictureBox);
+            Controls.Add(cameraPictureBox);
+            Name = "MainForm";
+            Text = "八宝粥检测";
+            ((System.ComponentModel.ISupportInitialize)cameraPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)detectedPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox cameraPictureBox;
+        private PictureBox detectedPictureBox;
+        private Button detectActionButton;
+        private Button selectPictureButton;
+        private OpenFileDialog openImageFileDialog;
+    }
+}
---
+++ 八宝粥识别/MainForm.cs
@@ -0,0 +1,82 @@
+using Microsoft.ML.Data;
+using TestConsole_ImageDetection;
+using static System.Windows.Forms.DataFormats;
+
+namespace 八宝粥识别;
+
+public partial class MainForm : Form
+{
+    public bool CameraMode { get; set; }
+    public string SelectedImagePath { get; set; } = string.Empty;
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void DetectActionButton_Click(object sender, EventArgs e)
+    {
+        //Load sample data
+        if (SelectedImagePath == string.Empty)
+        {
+            MessageBox.Show("无图像传入");
+            return;
+        }
+        var originImage = Image.FromFile(SelectedImagePath);
+        var image = MLImage.CreateFromFile(SelectedImagePath);
+        ImageDetectionTest.ModelInput sampleData = new ImageDetectionTest.ModelInput()
+        {
+            Image = image
+        };
+        //Load model and predict output
+        var result = ImageDetectionTest.Predict(sampleData);
+        var boxes = result.PredictedBoundingBoxes.Chunk(4)
+                                                 .Select(x => new { XTop = x[0], YTop = x[1], XBottom = x[2], YBottom = x[3] })
+                                                 .Zip(result.Score, (a, b) => new { Box = a, Score = b });
+        foreach (var box in boxes)
+        {
+            using var graphics = Graphics.FromImage(originImage);
+            #region 获取矩形位置
+            int x = (int)box.Box.XTop;
+            int y = (int)box.Box.YTop;
+            int width = (int)(box.Box.XBottom - box.Box.XTop);
+            int height = (int)(box.Box.YBottom - box.Box.YTop);
+            #endregion
+            int borderThickness = (int)(Math.Min(width, height) * 0.02);
+            float fontSize = Math.Min(width, height) * 0.1f;
+            var font = new Font(SystemFonts.DefaultFont.FontFamily, fontSize);
+            var fillBrush = new SolidBrush(GetTransparentColorForScore(box.Score));
+            var borderPen = new Pen(GetColorForScore(box.Score), borderThickness);
+            graphics.FillRectangle(fillBrush, x, y, width, height);
+            graphics.DrawRectangle(borderPen, x, y, width, height);
+            var confidenceText = $"置信度: {box.Score:F2}";
+            SizeF textSize = graphics.MeasureString(confidenceText, font);
+            graphics.FillRectangle(new SolidBrush(GetColorForScore(box.Score)), x - borderThickness / 2, y - textSize.Height - 5, textSize.Width, textSize.Height);
+            graphics.DrawString(confidenceText, font, Brushes.White, x - borderThickness / 2, y - textSize.Height - 5);
+        }
+        detectedPictureBox.Image = originImage;
+    }
+
+    private void SelectPictureButton_Click(object sender, EventArgs e)
+    {
+        if (openImageFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            SelectedImagePath = openImageFileDialog.FileName;
+        }
+        cameraPictureBox.Image = Image.FromFile(SelectedImagePath);
+    }
+
+    private static Color GetTransparentColorForScore(float score)
+    {
+        return Color.FromArgb(80, GetColorForScore(score));
+    }
+
+    private static Color GetColorForScore(float score)
+    {
+        if (score > 0.8)
+            return Color.Green;
+        else if (score > 0.5)
+            return Color.Yellow;
+        else
+            return Color.Red;
+    }
+}
