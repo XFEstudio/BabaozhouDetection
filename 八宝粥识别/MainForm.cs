e9db8c9e5d84b0c4fa291769ce88018d428bba42
XFEstudio <mail@xfegzs.com>
2023-12-21 02:21:42 +0800

    增加md文件

--- 八宝粥识别/MainForm.cs
+++ 八宝粥识别/MainForm.cs
@@ -15,45 +15,46 @@ public partial class MainForm : Form
 
     private void DetectActionButton_Click(object sender, EventArgs e)
     {
-        //Load sample data
-        if (SelectedImagePath == string.Empty)
+        Task.Run(() =>
         {
-            MessageBox.Show("无图像传入");
-            return;
-        }
-        var originImage = Image.FromFile(SelectedImagePath);
-        var image = MLImage.CreateFromFile(SelectedImagePath);
-        ImageDetectionTest.ModelInput sampleData = new ImageDetectionTest.ModelInput()
-        {
-            Image = image
-        };
-        //Load model and predict output
-        var result = ImageDetectionTest.Predict(sampleData);
-        var boxes = result.PredictedBoundingBoxes.Chunk(4)
-                                                 .Select(x => new { XTop = x[0], YTop = x[1], XBottom = x[2], YBottom = x[3] })
-                                                 .Zip(result.Score, (a, b) => new { Box = a, Score = b });
-        foreach (var box in boxes)
-        {
-            using var graphics = Graphics.FromImage(originImage);
-            #region 获取矩形位置
-            int x = (int)box.Box.XTop;
-            int y = (int)box.Box.YTop;
-            int width = (int)(box.Box.XBottom - box.Box.XTop);
-            int height = (int)(box.Box.YBottom - box.Box.YTop);
-            #endregion
-            int borderThickness = (int)(Math.Min(width, height) * 0.02);
-            float fontSize = Math.Min(width, height) * 0.1f;
-            var font = new Font(SystemFonts.DefaultFont.FontFamily, fontSize);
-            var fillBrush = new SolidBrush(GetTransparentColorForScore(box.Score));
-            var borderPen = new Pen(GetColorForScore(box.Score), borderThickness);
-            graphics.FillRectangle(fillBrush, x, y, width, height);
-            graphics.DrawRectangle(borderPen, x, y, width, height);
-            var confidenceText = $"置信度: {box.Score:F2}";
-            SizeF textSize = graphics.MeasureString(confidenceText, font);
-            graphics.FillRectangle(new SolidBrush(GetColorForScore(box.Score)), x - borderThickness / 2, y - textSize.Height - 5, textSize.Width, textSize.Height);
-            graphics.DrawString(confidenceText, font, Brushes.White, x - borderThickness / 2, y - textSize.Height - 5);
-        }
-        detectedPictureBox.Image = originImage;
+            if (SelectedImagePath == string.Empty)
+            {
+                MessageBox.Show("无图像传入");
+                return;
+            }
+            var originImage = Image.FromFile(SelectedImagePath);
+            var image = MLImage.CreateFromFile(SelectedImagePath);
+            ImageDetectionTest.ModelInput sampleData = new()
+            {
+                Image = image
+            };
+            var result = ImageDetectionTest.Predict(sampleData);
+            var boxes = result.PredictedBoundingBoxes.Chunk(4)
+                                                     .Select(x => new { XTop = x[0], YTop = x[1], XBottom = x[2], YBottom = x[3] })
+                                                     .Zip(result.Score, (a, b) => new { Box = a, Score = b });
+            foreach (var box in boxes)
+            {
+                using var graphics = Graphics.FromImage(originImage);
+                #region 获取矩形位置
+                int x = (int)box.Box.XTop;
+                int y = (int)box.Box.YTop;
+                int width = (int)(box.Box.XBottom - box.Box.XTop);
+                int height = (int)(box.Box.YBottom - box.Box.YTop);
+                #endregion
+                int borderThickness = (int)(Math.Min(width, height) * 0.01);//根据矩形大小调整边框粗细
+                float fontSize = Math.Min(width, height) * 0.1f;//根据矩形大小调整字体大小
+                var font = new Font(SystemFonts.DefaultFont.FontFamily, fontSize);
+                var fillBrush = new SolidBrush(GetTransparentColorForScore(box.Score));
+                var borderPen = new Pen(GetColorForScore(box.Score), borderThickness);
+                graphics.FillRectangle(fillBrush, x, y, width, height);
+                graphics.DrawRectangle(borderPen, x, y, width, height);
+                var confidenceText = $"相似度: {box.Score:F2}";
+                SizeF textSize = graphics.MeasureString(confidenceText, font);
+                graphics.FillRectangle(new SolidBrush(GetColorForScore(box.Score)), x - borderThickness / 2, y - textSize.Height - 5, textSize.Width, textSize.Height);
+                graphics.DrawString(confidenceText, font, Brushes.White, x - borderThickness / 2, y - textSize.Height - 5);
+            }
+            Invoke(() => detectedPictureBox.Image = originImage);
+        });
     }
 
     private void SelectPictureButton_Click(object sender, EventArgs e)
